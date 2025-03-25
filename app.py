ff09d25384d4c71ea24614462e64ccbb735d1612
Hamaxi <109148762+EgorStalker@users.noreply.github.com>
2025-03-25 10:09:00 +0200

    app.py

--- app.py
+++ app.py
@@ -1,64 +1,3 @@
-# from flask import Flask, render_template,request,render_template_string
-# from markupsafe import escape
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# @app.route('/home')
-# def index():
-#     return render_template("index.html")
-#
-#
-# @app.route('/about')
-# def index_about():
-#     return render_template("about.html")
-#
-#
-# @app.route("/ageFilter/<age>")
-# def about_us(age):
-#     if int(escape(age)) > 18:
-#         return "Bы можете зарегатся"
-#
-#     return "<h1>Bы ёще не готовы<h1/>"
-#
-# # @app.route("/login/<login>")
-# # def login(login):
-# #     if len(escape(login)) > 10:
-# #         return "Укоротите пароль"
-# #     elif len(escape(login))  <= 10 and len(escape(login)) != 0 :
-# #         return "Логин подходить "
-#
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return f'Post {post_id}'
-# # @app.route('/forum/<path:path>')
-# # def forum
-#
-#
-# def do_the_login():
-#     return "Logging in with POST method."
-#
-# def show_the_login_form():
-#     form = '''
-#     <form method="POST">
-#         Username: <input type="text" name="username"><br>
-#         Password: <input type="password" name="password"><br>
-#         <input type="submit" value="Login">
-#     </form>
-#     '''
-#     return form
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return do_the_login()
-#     else:
-#         return show_the_login_form()
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, jsonify
 from flask import request
 from flask import render_template_string,render_template
@@ -92,32 +31,12 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-#
-# @app.route('/books',methods=['GET'])
-# def get_books():
-#     books = Book.query.all()
-#
-#     books_data = [
-#         {
-#             'id':book.id,
-#             'name_book' : book.name_book,
-#             'year': book.year,
-#             'page' : book.page,
-#             'author_id' : book.author_id
-#
-#
-#         }
-#         for book in books
-#     ]
-#
-#     return jsonify(books_data)
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
-# @app.route("/register",methods = ['GET',"POST"])
-# def register():
-#     return render_template("register.html")
+
 def show_the_login_form():
     return render_template_string("""
         <form method="POST">
